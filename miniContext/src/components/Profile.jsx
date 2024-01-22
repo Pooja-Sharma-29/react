3f596285ab309fd87ee712867bbe73620398b665
Pooja <pooja.sharma7@cognizant.com>
2024-01-22 19:33:15 +0530

    context-api basics

---
+++ miniContext/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+  return user ? <div>Welcome {user.userName}</div> : <div>Please Login</div>;
+}
+
+export default Profile;
